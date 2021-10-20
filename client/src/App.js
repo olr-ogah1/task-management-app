8e55e3d654769a35254078713790eba772b848fc
Onyela Ogah <onyela.ogah@gmail.com>
2021-10-20 13:35:42 +0100

    Add styling

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import './App.css';
 import TaskForm from "./tasks/TaskForm";
 import TasksTable from "./tasks/TasksTable";
+import notepad from "./notepad.png";
 
 function App() {
   return (
     <div className="App container">
+      <h1>Organise everything with this Task Management App</h1>
+      <img src={notepad} alt="A notepad and pen"/>
+      <h3 >Add a task:</h3>
       <div>
-        <TaskForm />
+        <TaskForm/>
       </div>
+      <h3>List of tasks:</h3>
       <div>
         <TasksTable />
       </div>
